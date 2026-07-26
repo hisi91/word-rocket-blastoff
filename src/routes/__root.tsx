0d0555bde3d761092fd6622e288884f16fe2516d
yassine sihi <yassine@macbook-pro-4.home>
2026-07-26 21:14:48 +0200

    Replace favicon with Word Rocket W

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,6 +89,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       {
         rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
+        rel: "icon",
         type: "image/png",
         href: "/favicon.png",
       },
